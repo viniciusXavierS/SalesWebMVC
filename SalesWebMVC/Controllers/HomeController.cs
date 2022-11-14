2204ef94d450c7c2d1b29d12747540eab1c72490
viniciusXavierS <vinicius.silva@agilecontent.com>
2022-11-14 00:34:08 -0300

    First Model-Controller-View

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesWebMVC.Models;
-using System;
-using System.Collections.Generic;
+using SalesWebMVC.Models.ViewModels;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMVC.Controllers
 {
